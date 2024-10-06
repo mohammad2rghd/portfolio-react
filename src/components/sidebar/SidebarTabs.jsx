e3f3b04fa425f4374b2117d61950be9a4abeca9c
mohammad2rr <mohammad2rghdd@gmail.com>
2024-10-06 11:51:33 +0330

    bug fixed

--- src/components/sidebar/SidebarTabs.jsx
+++ src/components/sidebar/SidebarTabs.jsx
@@ -8,50 +8,46 @@ import MainContext from "../../context";
 import { tabsData } from "../../constants/tabsData";
 
 const SidebarTabs = () => {
-    const { pageNumber, handlePageNumber, setDrawerOpen } =
-        useContext(MainContext);
+  const { pageNumber, handlePageNumber, setDrawerOpen } =
+    useContext(MainContext);
 
-    const theme = useTheme();
-    const data = tabsData();
+  const theme = useTheme();
+  const data = tabsData();
 
-    return (
-        <Tabs
-            orientation="vertical"
-            variant="scrollable"
-            scrollButton="auto"
-            allowScrollButtonsMobile
-            value={pageNumber}
-            indicatorColor={
-                theme.palette.mode === "dark" ? "secondary" : "primary"
-            }
-            textColor={theme.palette.mode === "dark" ? "secondary" : "primary"}
-            onChange={handlePageNumber}
-        >
-            {data.map((tab, index) => (
-                <Tab
-                    key={index}
-                    label={tab.label}
-                    icon={tab.icon}
-                    iconPosition="start"
-                    sx={{
-                        backgroundColor:
-                            theme.palette.mode === "dark"
-                                ? grey[800]
-                                : grey[500],
-                        color: "text.primary",
-                        borderRadius: 2,
-                        my: 0.5,
-                        mx: 1,
-                        "&.MuiTab-root": {
-                            minHeight: 50,
-                        },
-                    }}
-                    onClick={() => setDrawerOpen(false)}
-                    {...tab}
-                />
-            ))}
-        </Tabs>
-    );
+  return (
+    <Tabs
+      orientation="vertical"
+      variant="scrollable"
+      scrollbutton="auto"
+      allowScrollButtonsMobile
+      value={pageNumber}
+      indicatorColor={theme.palette.mode === "dark" ? "secondary" : "primary"}
+      textColor={theme.palette.mode === "dark" ? "secondary" : "primary"}
+      onChange={handlePageNumber}
+    >
+      {data.map((tab, index) => (
+        <Tab
+          key={index}
+          label={tab.label}
+          icon={tab.icon}
+          iconPosition="start"
+          sx={{
+            backgroundColor:
+              theme.palette.mode === "dark" ? grey[800] : grey[500],
+            color: "text.primary",
+            borderRadius: 2,
+            my: 0.5,
+            mx: 1,
+            "&.MuiTab-root": {
+              minHeight: 50,
+            },
+          }}
+          onClick={() => setDrawerOpen(false)}
+          {...tab}
+        />
+      ))}
+    </Tabs>
+  );
 };
 
 export default SidebarTabs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,110 +8,102 @@ import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 
 import { links } from "../constants/particles";
-import bg02 from "../assets/bg02.jpeg";
+import bg03 from "../assets/bg03.jpeg";
 
 const Home = ({ helmetTitle }) => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    const nameEl = useRef(null);
-    const infoEl = useRef(null);
+  const nameEl = useRef(null);
+  const infoEl = useRef(null);
 
-    const strings = [
-        " توسعه دهنده فول استک هستم",
-        " مدرس برنامه نویسی هستم",
-        " فریلنسر هستم",
-        " محتواساز دنیای برنامه نویسی هستم",
-    ];
+  const strings = [
+    " توسعه دهنده فول استک هستم",
+    " مدرس برنامه نویسی هستم",
+    " فریلنسر هستم",
+    " محتواساز دنیای برنامه نویسی هستم",
+  ];
 
-    useEffect(() => {
-        const typedName = new Typed(nameEl.current, {
-            strings: [" یونس قربانی "],
-            typeSpeed: 110,
-            backSpeed: 80,
-            backDelay: 50,
-            showCursor: false,
-        });
+  useEffect(() => {
+    const typedName = new Typed(nameEl.current, {
+      strings: [" یونس قربانی "],
+      typeSpeed: 110,
+      backSpeed: 80,
+      backDelay: 50,
+      showCursor: false,
+    });
 
-        const stringsTransition = setInterval(() => {
-            setIndex((index) => index + 1);
-        }, 3000);
+    const stringsTransition = setInterval(() => {
+      setIndex((index) => index + 1);
+    }, 3000);
 
-        return () => {
-            typedName.destroy();
-            clearInterval(stringsTransition);
-        };
-    }, []);
+    return () => {
+      typedName.destroy();
+      clearInterval(stringsTransition);
+    };
+  }, []);
 
-    const particlesInit = useCallback(async (engine) => {
-        await loadFull(engine);
-    }, []);
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
 
-    const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
-    }, []);
-    return (
-        <Box
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+  return (
+    <Box
+      sx={{
+        backgroundImage: `url(${bg03})`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Helmet>
+        <title>{helmetTitle}</title>
+      </Helmet>
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={links}
+      />
+      <Box component="div" sx={{ display: "flex" }}>
+        <Typography variant="h3" color="#F93C92">
+          {"{{"}
+        </Typography>
+        <Typography ref={nameEl} variant="h3" color="tomato"></Typography>
+
+        <Typography variant="h3" color="#F93C92">
+          {"}}"}
+        </Typography>
+      </Box>
+
+      <Box component="div" sx={{ display: "flex" }}>
+        <TextTransition springConfig={presets.wobbly}>
+          <Typography
+            variant="h4"
+            color="whitesmoke"
             sx={{
-                backgroundImage: `url(${bg02})`,
-                backgroundPosition: "center",
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                height: "100vh",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
+              mt: 4,
+              textDecoration: "underline",
+              textDecorationColor: "#F93C92",
             }}
-        >
-            <Helmet>
-                <title>{helmetTitle}</title>
-            </Helmet>
-            <Particles
-                id="tsparticles"
-                init={particlesInit}
-                loaded={particlesLoaded}
-                options={links}
-            />
-            <Box component="div" sx={{ display: "flex" }}>
-                <Typography variant="h3" color="#F93C92">
-                    {"{{"}
-                </Typography>
-                <Typography
-                    ref={nameEl}
-                    variant="h3"
-                    color="tomato"
-                ></Typography>
+          >
+            {strings[index % strings.length]}
+          </Typography>
+        </TextTransition>
 
-                <Typography variant="h3" color="#F93C92">
-                    {"}}"}
-                </Typography>
-            </Box>
-
-            <Box component="div" sx={{ display: "flex" }}>
-                <TextTransition springConfig={presets.wobbly}>
-                    <Typography
-                        variant="h4"
-                        color="whitesmoke"
-                        sx={{
-                            mt: 4,
-                            textDecoration: "underline",
-                            textDecorationColor: "#F93C92",
-                        }}
-                    >
-                        {strings[index % strings.length]}
-                    </Typography>
-                </TextTransition>
-
-                <Typography
-                    variant="h4"
-                    color="whitesmoke"
-                    sx={{ mt: 4, mr: 1 }}
-                >
-                    من یک
-                </Typography>
-            </Box>
-        </Box>
-    );
+        <Typography variant="h4" color="whitesmoke" sx={{ mt: 4, mr: 1 }}>
+          من یک
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Home;
