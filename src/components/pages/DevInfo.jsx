aa397f764b0de299f987989c511b5882d029211e
mohammad2rr <mohammad2rghdd@gmail.com>
2024-10-06 14:04:10 +0330

    modified pages home and about us

--- src/components/pages/DevInfo.jsx
+++ src/components/pages/DevInfo.jsx
@@ -2,34 +2,29 @@ import { Typography } from "@mui/material";
 import { KeyboardArrowLeftRounded } from "@mui/icons-material";
 
 const Info = ({ children }) => {
-    return (
-        <Typography
-            variant="body1"
-            color="black"
-            textAlign="left"
-            sx={{ mt: 2 }}
-        >
-            {children}
-            <KeyboardArrowLeftRounded
-                sx={{
-                    verticalAlign: "bottom",
-                    color: "primary.main",
-                }}
-            />
-        </Typography>
-    );
+  return (
+    <Typography variant="body1" color="black" textAlign="left" sx={{ mt: 2 }}>
+      {children}
+      <KeyboardArrowLeftRounded
+        sx={{
+          verticalAlign: "bottom",
+          color: "primary.main",
+        }}
+      />
+    </Typography>
+  );
 };
 
 const DevInfo = () => {
-    return (
-        <>
-            <Info>نام و نام خانوادگی : یونس قربانی</Info>
-            <Info>سن : ۳۰</Info>
-            <Info>شهر : کلاله</Info>
-            <Info>Younes.Gh@Chmail.ir : آدرس ایمیل</Info>
-            <Info>شماره موبایل : ۰۹۳۵۰۰۰۱۱۲۲</Info>
-        </>
-    );
+  return (
+    <>
+      <Info>نام و نام خانوادگی : محمد قلاوند</Info>
+      <Info>سن : 29</Info>
+      <Info>شهر : تهران</Info>
+      <Info>mohammad2rghdd@gmail.com : آدرس ایمیل</Info>
+      <Info>شماره موبایل : </Info>
+    </>
+  );
 };
 
 export default DevInfo;
