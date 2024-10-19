1524088e702b9bd2d1748b1f8bec4160d9182cbd
mohammad2rr <mohammad2rghdd@gmail.com>
2024-10-19 12:54:06 +0330

    removed swipeable view package

--- src/containers/AppContainer.jsx
+++ src/containers/AppContainer.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 
 import { useMediaQuery } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
-import SwipeableViews from "react-swipeable-views";
 
 import MainLayout from "../layouts/MainLayout";
 import { Sidebar } from "../components/sidebar";
@@ -15,75 +14,70 @@ import { DrawerActionButton } from "../components/drawer";
 import { Home, About, Resume, Courses, Comments, Contact } from "../pages";
 
 function AppContainer() {
-    const [pageNumber, setPageNumber] = useState(0);
-    const [drawerOpen, setDrawerOpen] = useState(false);
-    const [mode, setMode] = useState();
+  const [pageNumber, setPageNumber] = useState(0);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [mode, setMode] = useState();
 
-    const theme = useTheme();
-    const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
-    const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useTheme();
+  const isMdUp = useMediaQuery(theme.breakpoints.up("md"));
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
-    useEffect(() => {
-        setMode(prefersDarkMode ? "dark" : "light");
-    }, []);
+  useEffect(() => {
+    setMode(prefersDarkMode ? "dark" : "light");
+  }, []);
 
-    useEffect(() => {
-        if (isMdUp) {
-            setDrawerOpen(false);
-        }
-    }, [isMdUp]);
+  useEffect(() => {
+    if (isMdUp) {
+      setDrawerOpen(false);
+    }
+  }, [isMdUp]);
 
-    const handlePageNumber = (event, newPage) => {
-        setPageNumber(newPage);
-    };
+  const handlePageNumber = (event, newPage) => {
+    setPageNumber(newPage);
+  };
 
-    const handleThemeChange = () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-    };
+  const handleThemeChange = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
 
-    return (
-        <MainContext.Provider
-            value={{
-                pageNumber,
-                handlePageNumber,
-                handleThemeChange,
-                drawerOpen,
-                setDrawerOpen,
-            }}
-        >
-            <MainLayout mode={mode}>
-                <SidebarContainer>
-                    <Sidebar />
-                </SidebarContainer>
-                <DrawerActionButton />
-                <PagesContainer>
-                    <SwipeableViews
-                        index={pageNumber}
-                        onChangeIndex={handlePageNumber}
-                    >
-                        <Page pageNumber={pageNumber} index={0}>
-                            <Home helmetTitle="وب سایت شخصی یونس قربانی" />
-                        </Page>
-                        <Page pageNumber={pageNumber} index={1}>
-                            <About helmetTitle="وب سایت شخصی | درباره من" />
-                        </Page>
-                        <Page pageNumber={pageNumber} index={2}>
-                            <Resume helmetTitle="وب سایت شخصی | رزومه من" />
-                        </Page>
-                        <Page pageNumber={pageNumber} index={3}>
-                            <Courses helmetTitle="وب سایت شخصی | دوره های من" />
-                        </Page>
-                        <Page pageNumber={pageNumber} index={4}>
-                            <Comments helmetTitle="وب سایت شخصی | نظرات دانشجویان" />
-                        </Page>
-                        <Page pageNumber={pageNumber} index={5}>
-                            <Contact helmetTitle="وب سایت شخصی | ارتباط با من" />
-                        </Page>
-                    </SwipeableViews>
-                </PagesContainer>
-            </MainLayout>
-        </MainContext.Provider>
-    );
+  return (
+    <MainContext.Provider
+      value={{
+        pageNumber,
+        handlePageNumber,
+        handleThemeChange,
+        drawerOpen,
+        setDrawerOpen,
+      }}
+    >
+      <MainLayout mode={mode}>
+        <SidebarContainer>
+          <Sidebar />
+        </SidebarContainer>
+        <DrawerActionButton />
+        <PagesContainer>
+          <Page pageNumber={pageNumber} index={0}>
+            <Home helmetTitle="وب سایت شخصی یونس قربانی" />
+          </Page>
+          <Page pageNumber={pageNumber} index={1}>
+            <About helmetTitle="وب سایت شخصی | درباره من" />
+          </Page>
+          <Page pageNumber={pageNumber} index={2}>
+            <Resume helmetTitle="وب سایت شخصی | رزومه من" />
+          </Page>
+          <Page pageNumber={pageNumber} index={3}>
+            <Courses helmetTitle="وب سایت شخصی | دوره های من" />
+          </Page>
+          <Page pageNumber={pageNumber} index={4}>
+            <Comments helmetTitle="وب سایت شخصی | نظرات دانشجویان" />
+          </Page>
+          <Page pageNumber={pageNumber} index={5}>
+            <Contact helmetTitle="وب سایت شخصی | ارتباط با من" />
+          </Page>
+        </PagesContainer>
+      </MainLayout>
+    </MainContext.Provider>
+  );
 }
 
 export default AppContainer;
